b57f99957d989bc47d55fc04cac6843fe336c644
Gamers-ID <75530021+Gamers-ID@users.noreply.github.com>
2020-12-26 10:24:41 +0700

    Add files via upload

---
+++ MFGAMERSv2.sh
@@ -0,0 +1,374 @@
+#!bin/bash
+#Buat apa recode? Izin Dlu Anjim
+#Recodelu_memang_nub
+#Semoga yang recode di kick dari keluarga amin dan jadi pengemis
+clear
+bi='\33[34;1m' #biru
+ij='\33[32;1m' #ijo
+pr='\33[35;1m' #purple
+cy='\33[36;1m' #cyan
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+echo $ku"Masukkan Nama Anda :"
+read nama
+echo "$cy Selamat Datang $nama"
+sleep 2
+clear
+echo $ku"Subs$cy Channel YT$ku Aing Dulu Cuk"
+sleep 2
+xdg-open https://youtube.com/channel/UCmBHO8a1jEVlYH5NIuAdfZA
+sleep 5
+clear
+echo "WAIT 3 SECONDS"
+     echo "Yang sabar dapat pahala amin..."
+sleep 3
+clear
+echo $me"       SUBSCRIBE LIKE AND COMMENT       "
+echo $ku"╭━─━─━─≪✠≫─━─━─━─━──━─━───━─━─≪✠≫─━─━─━╮"
+echo $pr"|  AUTHOR  :"$cy"MR.MF                      |"
+echo $pr"|  YOUTUBE :"$cy"MF GAMERS Official         |"
+echo $pr"|  INSTAGRAM :"$cy"mfgamers_yt              |"
+echo $ku"╰━─━─━─≪✠≫─━─━──━─━──━─━──━─━─≪✠≫─━─━─━╯"
+echo
+echo $ku "SILAHKAN PILIH NOMORNYA"
+echo $ku "___________________________________________"
+echo $cy "(1)200 TOOLS LENGKAP [ADA YANG COID]"
+echo $cy "(2)TOOLS MENGERIKAN [HARUS TAU KEGUNAANNYA]"
+echo $cy "(3)TOOLS SPAM CALL/SMS WA                  "
+echo $cy "(4)HACK FB                                 "
+echo $cy "(5)20 TOOLS HACKING TERBAIK                "
+echo $cy "(6)SMS-ID (FREE)                           "
+echo $cy "(7)SANTET ONLINE                           "
+echo $cy "(8)OSIF                                    "
+echo $cy "(9)VIRUS REAL (VBUG)                       "
+echo $cy "(10)MEMPERGANTENG TAMPILAN TERMUX          "
+echo $cy "(11)20 TOOLS HACKING TERBAIK VERSI 2       "
+echo $cy "(12)Zphisher (Phising)                     "
+echo $cy "(13)Weeman (Phising)                       "
+echo $cy "(14)DarkFly                                "
+echo $cy "(15)XERXES (COID)                          "
+echo $cy "(16)Script Nuyul Aplikasi                  "
+echo $cy "(17)Script Buat Para Jomblo :v             "
+echo $cy "(18)Spam Sms (Max 3× kirim)                "
+echo $cy "(19)Cam-Hackers (Hack Camera CCTV 91 Negara"
+echo $cy "(20)HiddenEye (Phising)                    "
+echo $cy "(0)EXIT                                    "
+echo $cy "___________________________________________"
+echo
+echo $cy "█▬█ █ ▀█▀"
+read -p "|░M░R░.░M░F░ PILIH: " pil :
+
+#batas
+if [ $pil = 1 ]
+then
+clear
+echo "Kalau di suruh masukan Username dan password, masukan saja"
+sleep 51
+echo "Username : BAJINGAN"
+sleep 5
+echo "Password : Gans"
+sleep 5
+echo "Ingatin itu!!!"
+sleep 1
+clear
+apt update
+apt upgrade
+apt install git
+apt install php
+apt install figlet
+apt install toilet
+pip2 install requests
+pip2 install termcolor
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+#batas
+if [ $pil = 2 ]
+then
+clear
+echo "Username : Abyss-Walker"
+sleep 5
+echo "Password : UchihaClan"
+sleep 5
+echo "Ingatkan!!!"
+sleep 2
+clear
+git clone https://github.com/AbyssExploit/Uchiha
+cd Uchiha
+bash setup.sh
+bash sharingan.sh
+fi
+#batas
+if [ $pil = 3 ]
+then
+pkg update && pkg upgrade
+pkg install python2
+pkg install python
+pkg install git
+pkg install clang
+pip install mechanize
+pip install requests
+pip install bs4
+git clone https://github.com/termuxandi/tollsSpam
+cd tollsSpam
+sh SpamNmr.sh
+fi
+#batas
+if [ $pil = 4 ]
+then
+clear
+echo $ku"Username : anggaxd"
+sleep 5
+echo $ku"Password : c-all"
+sleep 5
+echo $cy"IngatKan itu!!!"
+sleep 2
+clear
+apt update && apt upgrade
+pkg install git
+pkg install python2
+pkg install pip2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/anggaxd/cid
+cd cid
+python2 crack.py
+fi
+#batas
+if [ $pil = 5 ]
+then
+clear
+echo $ku"ID : subscribe"
+sleep 5
+echo $ku"Password : channel pajaoq"
+sleep 5
+echo $ku"Ingatkan!!!"
+sleep 2
+clear
+apt update && apt upgrade
+pkg install python2
+pkg install git
+pkg install python
+pkg install bash
+pkg install php
+git clone https://github.com/novalattasya/Tool-N
+cd Tool-N
+pip2 install requests
+pip2 install mechanize
+python2 v1.py
+fi
+#batas
+if [ $pil = 6 ]
+then
+clear
+figlet SMSID BY Gamers-ID
+sleep 3
+clear
+echo $ku"Subs$cy Channel YT$ku Aing Dulu Cuk"
+sleep 2
+xdg-open https://youtube.com/channel/UCmBHO8a1jEVlYH5NIuAdfZA
+sleep 6
+smsid boom -y
+fi
+#batas
+if [ $pil = 7 ]
+then
+apt update && apt upgrade
+apt install git
+apt install python2
+apt install python
+pip2 install requests
+git clone https://github.com/Gameye98/santet-online
+santet-online
+chmod +x *
+python2 santet.py
+fi
+#batas
+if [ $pil = 8 ]
+then
+clear
+pkg update && pkg upgrade
+pip2 install requests
+git clone https://github.com/Ciku370/OSIF
+cd OSIF
+python2 osif.py
+fi
+#batas
+if [ $pil = 9 ]
+then
+git clone https://github.com/Junior60/vbug
+cd vbug
+python2 vbug.py
+fi
+#batas
+if [ $pil = 10 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install nano
+git clone https://github.com/Din-zUgex95/style
+cd style
+python2 termux.py
+fi
+#batas
+if [ $pil = 11 ]
+then
+clear
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install php
+pip install requests
+pip install mechanize
+pip install bs4
+git clone https://github.com/Bhayou/tools-bhayou
+cd tools-bhayou
+sh tools-bhayou.sh
+fi
+#batas
+if [ $pil = 12 ]
+then
+apt update && apt install git php curl openssh -y && git clone https://github.com/htr-tech/zphisher && cd zphisher && chmod +x zphisher.sh && bash zph>
+fi
+#batas
+if [ $pil = 13 ]
+then
+clear
+echo $cy"Cara pemakaian"
+sleep 2
+echo $cy"Kalau sudah masuk ke tools nya ketik> set url (Link web untuk di samarkan) <"
+sleep 2
+echo $cy"Lalu ketik> set action_url (link web untuk di samarkan) <"
+sleep 1
+echo $cy"Bagian terakhir Ketik> run <"
+sleep 1
+echo $cy"Ingat Itu!!!"
+sleep 5
+clear
+apt update
+apt upgrade
+apt install python2
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x *
+python2 weeman.py
+fi
+#batas
+if [ $pil = 14 ]
+then
+clear
+apt update && apt upgrade
+pkg install python2
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+chmod +x install.py
+cd
+DarkFly
+fi
+#batas
+if [ $pil = 15 ]
+then
+clear
+echo $me"COID$cy BRO"
+sleep 3
+exit
+fi
+#batas
+if [ $pil = 16 ]
+then
+clear
+pkg install nano
+pkg install php
+pkg install python
+git clone https://github.com/MaulanaRyM/Nuyul-Apk
+cd Nuyul-Apk
+sh bot.sh
+fi
+#batas
+if [ $pil = 17 ]
+then
+clear
+apt update && apt upgrade
+pkg install python git
+pip install requests
+git clone https://github.com/Sxp-ID/bot-chat
+cd bot-chat
+python bot.py
+fi
+#batas
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+pkg install python git
+pip install requests bs4
+git clone https://github.com/Sxp-ID/spam-smsv2
+cd spam-smsv2
+python sms.py
+fi
+#batas
+if [ $pil = 19 ]
+then
+clear
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+pip install requests
+pip install colorama
+mv Cam-Hackers $HOME
+cd $HOME/Cam-Hackers
+python cam-hackers.py
+fi
+#batas
+if [ $pil = 20 ]
+then
+clear
+echo $cy"TUNGGU PENGINSTALAN, AGAK LAMA"
+sleep 2
+clear
+git clone https://github.com/Open-Security-Group-OSG/HiddenEyeReborn
+cd HiddenEyeReborn
+pip install -r requirements.txt
+pip install hiddeneye-reborn
+python setup.py install
+fi
+#batas
+if [ $pil = 0 ]
+then
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+echo $cy"Thanks Sudah Pakai Script Ini"
+sleep 1
+clear
+figlet Bye Bye | lolcat
+sleep 2
+exit
+fi
